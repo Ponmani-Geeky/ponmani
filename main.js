f9ee58ee03de0e31e1b4013b694f19d0bfca6d80
Ponmani <ponmani529@gmail.com>
2021-04-06 19:38:57 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const toggle_btn = document.querySelector(".toggle-button");
+const navbarlink = document.querySelector(".navbar_links");
+const navbar = document.querySelector(".navbar");
+
+toggle_btn.addEventListener("click", () => {
+  navbarlink.classList.toggle("active");
+});
+
+const texts = ["Software Engineer", "Frontend Enthusiastic", "Avid Learner"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count == texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".main_text").textContent = letter;
+  if (letter.length == currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 400);
+})();
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    navbar.classList.add("active");
+  } else {
+    navbar.classList.remove("active");
+  }
+}
